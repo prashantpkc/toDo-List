1e323e572175e78a63bdd71dca7010799472022f
prashantpkc <119597273+prashantpkc@users.noreply.github.com>
2023-05-30 09:48:53 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React, {useState} from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const [inputValue, setInputvalue] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleAdd = () => {
+    setTodos([...todos, inputValue]);
+    setInputvalue('');
+  };
+
+  const handleInputChange = (e) => {
+    setInputvalue(e.target.value);
+  };
+  const handleDelete = (index) => {
+    let temp = [...todos];
+    // temp.splice(index, 1);
+   let res = temp.filter((ele,ind)=> ind !== index)
+    setTodos(res)
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>toDo List!</h1>
+      <div>
+        <input type="text" onChange={handleInputChange} value={inputValue} />
+
+        <button onClick={handleAdd}>Add</button>
+        {todos.map((todo, index) => {
+          return (
+            <ul>
+              <li key={index}>{todo}</li>
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </ul>
+          );
+        })}
+      </div>
     </div>
   );
 }
